477773e9f70ee6d96083d6c2301b2edd8c944e7e
sidharthk00 <sidharthkrishnan.00@gmail.com>
2022-08-24 11:25:59 +0530

    new project update

---
+++ script1.js
@@ -0,0 +1,26 @@
+var todos = [
+  "wake up early",
+  "make breskfast",
+  "brush teeth",
+  "exercise",
+  "read book",
+  "study javascript",
+]
+var new1 = [
+  "wake up early1",
+  "make breskfast1",
+  "brush teeth1",
+  "exercise1",
+  "read book1",
+  "study javascript1",
+]
+
+//for( var i = 0; i < todos.length; i++)
+//  { console.log(todos[i] = todos[i] + "!!")
+
+
+ // }
+  new1.forEach(function(t){
+  	console.log(t);
+  })
+
